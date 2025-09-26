16bd655ace8d08a38e589f24b4e77a67d9df7a5a
ahhfzwl <ahhfzwl@qq.com>
2025-09-26 15:44:47 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 export LANG=en_US.UTF-8
 export uuid=${UUID:-''}
-export domain=${DOMAIN:-''}
 export vl_port=${PORT:-''}
 wget https://raw.githubusercontent.com/ahhfzwl/node.js/main/app.js
 wget https://raw.githubusercontent.com/ahhfzwl/node.js/main/package.json
 sed -i "s/('UUID', '')/('UUID', '$uuid')/g" "app.js"
-sed -i "s/('DOMAIN', '')/('DOMAIN', '$domain')/g" "app.js"
 sed -i "s/('PORT', '')/('PORT', '$vl_port')/g" "app.js"
 npm start
